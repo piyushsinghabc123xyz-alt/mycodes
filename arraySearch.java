b47dda8ff365d9116b2853c2055f781168cd32db
Piyush <piyush.singh.abc.123.xyz@gmail.com>
2026-07-03 20:09:05 +0530

    Clarify linear search comment in arraySearch.java
    Added a comment to clarify the purpose of the linear search section.

--- arraySearch.java
+++ arraySearch.java
@@ -5,10 +5,10 @@ public class arraySearch {
     public static void main(String []args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Size of array:");
-        int size = sc.nextInt();
+        int size = sc.nextInt();        
         int numbers[]= new int[size];
 
-        System.out.println("Enter elements of array one by one :");             // **** LINEAR SEARCH ****
+        System.out.println("Enter elements of array one by one :");       // **** LINEAR SEARCH ****  (Searching a given element in array)
         for(int i=0;i<numbers.length;i++){
             numbers [i] = sc.nextInt();
         }
