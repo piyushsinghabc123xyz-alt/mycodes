c07af7ac67271c0ddf6caa058db26115d526a444
Piyush <piyush.singh.abc.123.xyz@gmail.com>
2026-07-03 20:22:49 +0530

    Add comment for matrix initialization purpose
    Added a comment to clarify the purpose of the matrix initialization.

--- matrixSearch.java
+++ matrixSearch.java
@@ -10,7 +10,7 @@ public class matrixSearch {
         System.out.println("Enter no of columns");
         int cols = sc.nextInt();
 
-        int[][] matrix = new int[rows][cols];
+        int[][] matrix = new int[rows][cols];                    // Searching an element in matrix and returning its index
 
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
