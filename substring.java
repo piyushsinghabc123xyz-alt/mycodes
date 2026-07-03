b9e960a87f38de39f00c61ce3042219cdd46b0ef
Piyush <piyush.singh.abc.123.xyz@gmail.com>
2026-07-03 20:44:48 +0530

    Update substring.java

--- substring.java
+++ substring.java
@@ -2,7 +2,7 @@ package Mycodes;
 
 public class substring {
     public static void main(String[]args){
-        String name = "PiyushSingh";
+        String name = "PiyushSingh";                      // Printing substring from a string
 
         System.out.println(name.substring(0,6));
     }
