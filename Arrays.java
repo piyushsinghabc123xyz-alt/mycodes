eef578401ab260e976f9fe1f5e739d71cf797a0c
Piyush <piyush.singh.abc.123.xyz@gmail.com>
2026-07-03 20:03:08 +0530

    Clarify array creation with a comment
    Added comment to clarify array creation.

--- Arrays.java
+++ Arrays.java
@@ -2,7 +2,7 @@ package Mycodes;
 import java.util.*;
 public class Arrays {
     public static void main(String[]args){
-        int[] marks = new int[3];
+        int[] marks = new int[3];                //Creating an array of size -> 3
         Scanner sc = new Scanner(System.in);
         marks[0]= sc.nextInt();
         marks[1]= sc.nextInt();
