7ac9cfa8ac0e78cf785211580c94ab1ef1510984
piyushsinghabc123xyz-alt <piyush.singh.abc.123.xyz@gmail.com>
2026-07-03 19:43:16 +0530

    Making a substring from a String

---
+++ substring.java
@@ -0,0 +1,9 @@
+package Mycodes;
+
+public class substring {
+    public static void main(String[]args){
+        String name = "PiyushSingh";
+
+        System.out.println(name.substring(0,6));
+    }
+}
