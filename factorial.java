b46eaf12292a5ba9a4766f3e60f3ca8b2ed6c747
Piyush <piyush.singh.abc.123.xyz@gmail.com>
2026-07-03 20:14:11 +0530

    Clarify factorial calculation with a comment
    Added a comment to clarify the factorial calculation.

--- factorial.java
+++ factorial.java
@@ -8,11 +8,11 @@ public class factorial {
         }
         int factorial = 1;
         for (int i=n;i>=1;i--){
-            factorial = factorial * i;
+            factorial = factorial * i;                  // Function for factorial of a no n taken input by the user
         }
         System.out.println(factorial);
     }
-    public static void main(String[] args){
+    public static void main(String[] args){    
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         printFactorial(n);
