a752112852f77e70a68717f4dc9225d85ce09412
piyushsinghabc123xyz-alt <piyush.singh.abc.123.xyz@gmail.com>
2026-07-09 01:47:47 +0530

    adding updatebit code to my repo

---
+++ updateBit.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class updateBit {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int n = 5; // = 0101
+        System.out.println("Enter bit to update :");
+        int bit = sc.nextInt();
+
+        int bitmask = 1<<bit-1;
+
+        if((n & bitmask) == 0){     //set
+            System.out.println(bitmask | n);
+        }else{                  //clear
+            System.out.println(~(bitmask)&n);
+        }
+    }
+}
