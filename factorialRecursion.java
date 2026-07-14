44853e59c33f6d3d86ae331eb047a23dd786311b
piyushsinghabc123xyz-alt <piyush.singh.abc.123.xyz@gmail.com>
2026-07-15 01:44:42 +0530

    more optimized factorial code

--- factorialRecursion.java
+++ factorialRecursion.java
@@ -1,19 +1,15 @@
 public class factorialRecursion {
-    public static void printFactorial(int n,int mult,int i){
-        if(n==0){
-            System.out.println("1");
-            return;
+    public static int printFactorial(int n){
+        if(n==1 ||n==0){
+            return 1;
         }
-        else if(i==n){
-            mult= mult*i;
-            System.out.println(mult);
-            return;
-        }
-        mult = mult*i;
-        printFactorial(n, mult, i+1);
+        int fact_nm1 = printFactorial(n-1);
+        int fact_n = fact_nm1*n;
+        return fact_n;
     }
     public static void main(String[]main){
-        int n = 0;
-        printFactorial(n,1,1);
+        int n = 5;
+        int factorial = printFactorial(n);
+        System.out.println(factorial);
     }
 }
