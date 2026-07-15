f0b6b40699f3b4e64529f4989f76f06e112d92e6
piyushsinghabc123xyz-alt <piyush.singh.abc.123.xyz@gmail.com>
2026-07-16 01:10:46 +0530

    fibonacci recursion code

---
+++ fibonacciRecursion.java
@@ -0,0 +1,17 @@
+public class fibonacciRecursion{
+    public static  void printFibo(int a,int b, int n){
+        if(n==0||n<0){
+            return;
+        }
+        int c = a+b;
+        System.out.println(c);
+        printFibo(b,c,n-1);
+    }
+    public static void main(String[]args){
+        int a = 0, b = 1;
+        System.out.println(a);
+        System.out.println(b);
+        int n = 8;
+        printFibo(a, b, n-2);
+    }
+}
